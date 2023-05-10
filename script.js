994bfa8482f63a978cd0887714435c66ec3b5deb
username <youremail@domain.com>
2023-05-10 09:13:04 -0400

    Add hover effect for playerBoats placement

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 let play = document.getElementsByClassName("play");
 let reset = document.getElementsByClassName("reset")[0];
 let two = document.querySelector(".two");
-let three = document.getElementsByClassName("three");
+let three = document.querySelector(".three");
 let threeTwo = document.getElementsByClassName("three-2");
 let four = document.getElementsByClassName("four");
 let rotate = document.getElementsByClassName("rotate");
 let playerBoard = document.getElementsByClassName("player-board");
 let computerBoard = document.getElementsByClassName("computer-board");
+const el = document.getElementsByClassName("player-board")[0];
 
 reset.addEventListener('click', function () {
   location.reload();
@@ -18,6 +19,7 @@ const boardSize = 8;
 const board = [];
 let playerShips = [];
 let computerShips = [];
+let hold;
 
 // Create/initiate the board
 for (let row = 0; row < boardSize; row++) {
@@ -62,6 +64,7 @@ function makeShips() {
 }
 
 
+
 function placeShips() {
   for (let i = 0; i < computerShips.length; i++) {
     x = Math.floor(Math.random() * 4) + 1;
@@ -83,9 +86,69 @@ function placeShips() {
   }
 }
 
-const el = document.getElementsByClassName("player-board")[0];
+let isClicked = {
+  twoClicked: false,
+  threeClicked: false,
+  threeTwoClicked: false,
+  fourClicked: false
+}
+
+
+function shipHoverEffect() {
+  el.addEventListener('mouseover', function (e) {
+    let x = e.target.id;
+    let y = x.split("");
+    let z = y[0].charCodeAt(0) - 65;
+    let w = y[1] - 1;
+    if (hold === 2) {
+    let perp = document.getElementById(String.fromCharCode(65 + (z + 1)) + (w + 1));
+    e.target.style.backgroundColor = "#525252";
+    perp.style.backgroundColor = "#525252";
+    } else if (hold === 3) {
+    let perp = document.getElementById(String.fromCharCode(65 + (z + 1)) + (w + 1));
+    let perp1 = document.getElementById(String.fromCharCode(65 + (z + 2)) + (w + 1));
+    e.target.style.backgroundColor = "#525252";
+    perp.style.backgroundColor = "#525252";
+    perp1.style.backgroundColor = "#525252";
+    } else if (hold === 4) {
+    let perp = document.getElementById(String.fromCharCode(65 + (z + 2)) + (w + 1));
+    let perp1 = document.getElementById(String.fromCharCode(65 + (z + 3)) + (w + 1));
+    let perp2 = document.getElementById(String.fromCharCode(65 + (z + 1)) + (w + 1));
+    perp.style.backgroundColor = "#525252";
+    perp1.style.backgroundColor = "#525252";
+    perp2.style.backgroundColor = "#525252";
+  }});
+
+  el.addEventListener('mouseout', function (e) {
+    let x = e.target.id;
+    let y = x.split("");
+    let z = y[0].charCodeAt(0) - 65;
+    let w = y[1] - 1;
+    if (hold === 2) {
+      let perp = document.getElementById(String.fromCharCode(65 + (z + 1)) + (w + 1));
+      e.target.style.backgroundColor = "#bcbcbc";
+      perp.style.backgroundColor = "#bcbcbc";
+      } else if (hold === 3) {
+      let perp = document.getElementById(String.fromCharCode(65 + (z + 1)) + (w + 1));
+      let perp1 = document.getElementById(String.fromCharCode(65 + (z + 2)) + (w + 1));
+      e.target.style.backgroundColor = "#bcbcbc";
+      perp.style.backgroundColor = "#bcbcbc";
+      perp1.style.backgroundColor = "#bcbcbc";
+      } else if (hold === 4) {
+      let perp = document.getElementById(String.fromCharCode(65 + (z + 2)) + (w + 1));
+      let perp1 = document.getElementById(String.fromCharCode(65 + (z + 3)) + (w + 1));
+      let perp2 = document.getElementById(String.fromCharCode(65 + (z + 1)) + (w + 1));
+      perp.style.backgroundColor = "#bcbcbc";
+      perp1.style.backgroundColor = "#bcbcbc";
+      perp2.style.backgroundColor = "#bcbcbc";
+}});
+}
+
 
 two.addEventListener('click', function () {
+  hold = 2;
+  if (isClicked.twoClicked === false) {
+  shipHoverEffect();
   el.addEventListener('click', function (e) {
     let x = e.target.id;
     let y = x.split("");
@@ -93,12 +156,87 @@ two.addEventListener('click', function () {
     let w = y[1] - 1;
     playerShips[0].position[0] = x;
     playerShips[0].position[1] = String.fromCharCode(65 + (z + 1)) + (w + 1);
-    e.target.style.backgroundColor = "#525252";
-  });  
-});
+    let perp = document.getElementById(playerShips[0].position[1]);
+    console.log(e.target)
+    // e.target.style.backgroundColor = "#af1111";
+    // perp.style.backgroundColor = "#af1111";
+    isClicked.twoClicked = true;
+
+  });
+  two.style.backgroundColor = "#525252";
+  two.style.border = "5px solid #000000";
+  } else {
+    alert("You've already placed this ship!");
+}});
+
+three.addEventListener('click', function () {
+  hold = 3;
+  console.log(hold);
+  if (isClicked.threeClicked === false) {
+  shipHoverEffect();
+  el.addEventListener('click', function (e) {
+    let x = e.target.id;
+    let y = x.split("");
+    let z = y[0].charCodeAt(0) - 65;
+    let w = y[1] - 1;
+    playerShips[1].position[0] = x;
+    playerShips[1].position[1] = String.fromCharCode(65 + (z + 1)) + (w + 1);
+    playerShips[1].position[2] = String.fromCharCode(65 + (z + 2)) + (w + 1);
+    let perp = document.getElementById(playerShips[1].position[1]);
+    let perp1 = document.getElementById(playerShips[1].position[2]);
+    e.target.style.backgroundColor = "#af1111";
+    perp.style.backgroundColor = "#af1111";
+    perp1.style.backgroundColor = "#af1111";
+    isClicked.threeClicked = true;
+  })
+  three.style.backgroundColor = "#525252";
+  three.style.border = "5px solid #000000";
+}});
 
 
+function checkAllShipsPlaced() {
+  for (let i = 0; i < playerShips.length; i++) {
+    if (playerShips[i].position.includes(null)) {
+      return false;
+    }
+  }
+  return true;
+};
 
+function checkShipHit(ship, coordinate) {
+  for (let i = 0; i < ship.position.length; i++) {
+    if (ship.position[i] === coordinate) {
+      ship.position[i] = "hit";
+      return true;
+    }
+  }
+  return false;
+}
+
+function checkShipSunk(ship) {
+  for (let i = 0; i < ship.position.length; i++) {
+    if (ship.position[i] !== "hit") {
+      return false;
+    }}
+  ship.sunk = true;
+  return true;
+  };
+
+
+function checkGameOver() {
+    for (let i = 0; i < playerShips.length; i++) {
+      if (playerShips[i].sunk = false) {
+        return false;
+      }
+    }
+    for (let i = 0; i < computerShips.length; i++) {
+      if (computerShips[i].sunk = false) {
+        return false;
+      }
+    }
+    return true;
+}
+  
 
 
 

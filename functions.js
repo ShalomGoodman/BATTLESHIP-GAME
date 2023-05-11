78fcace75150cacc80cff9642f680cfd6b138593
username <youremail@domain.com>
2023-05-11 11:09:07 -0400

    Functionality complete, but still major bugs to fix

--- functions.js
+++ functions.js
@@ -1,9 +1,9 @@
+let playerShipArray = [];
+let computerShipArray = [];
 // Creates and Initializes the game board.
 function createBoard() {
-  console.log("test")
     for (let row = 0; row < boardSize; row++) {
       const currentRow = [];
-    
       for (let col = 0; col < boardSize; col++) {
         const coordinate = String.fromCharCode(65 + col) + (row + 1);
         currentRow.push(coordinate);
@@ -19,10 +19,10 @@ function createBoard() {
 
 // Class constructor for the ships.
 class Ship {
-    constructor(size, a, b, c = null, d = null) {
+    constructor(size, a, b, ) {
         this.size = size; // Size of the ship (e.g. 2 = ["A1", "A2"]).
         this.sunk = false; // Will dictate the color of the ship and update score.
-        this.position = [a, b, c, d]; // Array of coordinates (e.g. ["A1", "A2", "A3"]).
+        this.position = [a, b]; // Array of coordinates (e.g. ["A1", "A2", "A3"]).
 }}
 
 // Makes ships for both the player and computer.
@@ -43,71 +43,80 @@ function makeShips() {
 // Automatically places the computer's ships.
 function placeShips() {
     for (let i = 0; i < computerShips.length; i++) {
-      x = Math.floor(Math.random() * 4) + 1;
-      y = String.fromCharCode(65 + Math.floor(Math.random() * 8));
-      let el = computerShips[i]
-      if (computerShips[i].size === 2) {
-      el.position[0] = y + x;
-      el.position[1] = y + (x + 1);
-      } else if (computerShips[i].size === 3) {
-      el.position[0] = y + x;
-      el.position[1] = y + (x + 1);
-      el.position[2] = y + (x + 2);
-      } else if (computerShips[i].size === 4) {
-      el.position[0] = y + x;
-      el.position[1] = y + (x + 1);
-      el.position[2] = y + (x + 2);
-      el.position[3] = y + (x + 3);
+      coordinateNumber = Math.floor(Math.random() * 4) + 1;
+      coordinateLetter = String.fromCharCode(65 + Math.floor(Math.random() * 8));
+      let cS = computerShips[i]
+      cS.position[0] = coordinateLetter + coordinateNumber;
+      cS.position[1] = coordinateLetter + (coordinateNumber + 1);
+      computerShipArray.push(cS.position[0], cS.position[1]);
+    if (computerShips[i].size === 3) {
+      cS.position[2] = coordinateLetter + (coordinateNumber + 2);
+      computerShipArray.push(cS.position[2]);
+    } else if (computerShips[i].size === 4) {
+      cS.position[2] = coordinateLetter + (coordinateNumber + 2);
+      cS.position[3] = coordinateLetter + (coordinateNumber + 3);
+      computerShipArray.push(cS.position[2], cS.position[3]);
 }}};
 
 // Shows the phantoms of the ships on the player's board during hover.
-function shipHoverEffect() {
-    el.addEventListener('mouseover', function (e) {
-      let x = e.target.id;
-      let y = x.split("");
-      let z = y[0].charCodeAt(0) - 65;
-      let w = y[1] - 1;
-      if (hold === 2) {
-      let perp = document.getElementById(String.fromCharCode(65 + (z + 1)) + (w + 1));
-      e.target.style.backgroundColor = "#525252";
-      perp.style.backgroundColor = "#525252";
-      } else if (hold === 3) {
-      let perp = document.getElementById(String.fromCharCode(65 + (z + 1)) + (w + 1));
-      let perp1 = document.getElementById(String.fromCharCode(65 + (z + 2)) + (w + 1));
-      e.target.style.backgroundColor = "#525252";
-      perp.style.backgroundColor = "#525252";
-      perp1.style.backgroundColor = "#525252";
-      } else if (hold === 4) {
-      let perp = document.getElementById(String.fromCharCode(65 + (z + 2)) + (w + 1));
-      let perp1 = document.getElementById(String.fromCharCode(65 + (z + 3)) + (w + 1));
-      let perp2 = document.getElementById(String.fromCharCode(65 + (z + 1)) + (w + 1));
-      perp.style.backgroundColor = "#525252";
-      perp1.style.backgroundColor = "#525252";
-      perp2.style.backgroundColor = "#525252";
-    }});
-    el.addEventListener('mouseout', function (e) {
-      let x = e.target.id;
-      let y = x.split("");
-      let z = y[0].charCodeAt(0) - 65;
-      let w = y[1] - 1;
-      if (hold === 2) {
-        let perp = document.getElementById(String.fromCharCode(65 + (z + 1)) + (w + 1));
-        e.target.style.backgroundColor = "#bcbcbc";
-        perp.style.backgroundColor = "#bcbcbc";
-        } else if (hold === 3) {
-        let perp = document.getElementById(String.fromCharCode(65 + (z + 1)) + (w + 1));
-        let perp1 = document.getElementById(String.fromCharCode(65 + (z + 2)) + (w + 1));
-        e.target.style.backgroundColor = "#bcbcbc";
-        perp.style.backgroundColor = "#bcbcbc";
-        perp1.style.backgroundColor = "#bcbcbc";
-        } else if (hold === 4) {
-        let perp = document.getElementById(String.fromCharCode(65 + (z + 2)) + (w + 1));
-        let perp1 = document.getElementById(String.fromCharCode(65 + (z + 3)) + (w + 1));
-        let perp2 = document.getElementById(String.fromCharCode(65 + (z + 1)) + (w + 1));
-        perp.style.backgroundColor = "#bcbcbc";
-        perp1.style.backgroundColor = "#bcbcbc";
-        perp2.style.backgroundColor = "#bcbcbc";
-}})};
+// function shipHoverEffect(el) {
+//   if (buttonClicked === false) {
+//   el.addEventListener('mouseover', function (e) {
+//     let x = e.target.id;
+//     console.log("here")
+//     let y = x.split("");
+//     let z = y[0].charCodeAt(0) - 65;
+//     let w = y[1] - 1;
+//   if (hold === 2) {
+//     let perp = document.getElementById(String.fromCharCode(65 + (z + 1)) + (w + 1));
+//     e.target.style.backgroundColor = "white";
+//     perp.style.backgroundColor = "white";
+//   } else if (hold === 3) {
+//     let perp = document.getElementById(String.fromCharCode(65 + (z + 1)) + (w + 1));
+//     let perp1 = document.getElementById(String.fromCharCode(65 + (z + 2)) + (w + 1));
+//     e.target.style.backgroundColor = "#525252";
+//     perp.style.backgroundColor = "#525252";
+//     perp1.style.backgroundColor = "#525252";
+//   } else if (hold === 4) {
+//     let perp = document.getElementById(String.fromCharCode(65 + (z + 2)) + (w + 1));
+//     let perp1 = document.getElementById(String.fromCharCode(65 + (z + 3)) + (w + 1));
+//     let perp2 = document.getElementById(String.fromCharCode(65 + (z + 1)) + (w + 1));
+//     perp.style.backgroundColor = "#525252";
+//     perp1.style.backgroundColor = "#525252";
+//     perp2.style.backgroundColor = "#525252";
+//   }});
+
+//   el.addEventListener('mouseout', function (e) {
+//     let x = e.target.id;
+//     let y = x.split("");
+//     let z = y[0].charCodeAt(0) - 65;
+//     let w = y[1] - 1;
+//     if (hold === 2) {
+//       let perp = document.getElementById(String.fromCharCode(65 + (z + 1)) + (w + 1));
+//       console.log(e.target.style.backgroundColor)
+//       e.target.style.backgroundColor = "red";
+//       perp.style.backgroundColor = "red";
+//     } else if (hold === 3) {
+//       let perp = document.getElementById(String.fromCharCode(65 + (z + 1)) + (w + 1));
+//       let perp1 = document.getElementById(String.fromCharCode(65 + (z + 2)) + (w + 1));
+//       e.target.style.backgroundColor = "#bcbcbc";
+//       perp.style.backgroundColor = "#bcbcbc";
+//       perp1.style.backgroundColor = "#bcbcbc";
+//     } else if (hold === 4) {
+//       let perp = document.getElementById(String.fromCharCode(65 + (z + 2)) + (w + 1));
+//       let perp1 = document.getElementById(String.fromCharCode(65 + (z + 3)) + (w + 1));
+//       let perp2 = document.getElementById(String.fromCharCode(65 + (z + 1)) + (w + 1));
+//       perp.style.backgroundColor = "#bcbcbc";
+//       perp1.style.backgroundColor = "#bcbcbc";
+//       perp2.style.backgroundColor = "#bcbcbc";
+// }
+
+// });
+//   } else {
+//     console.log("here2");
+//   }
+
+// }
 
 // makes the ship buttons look like they were clicked.
 function buttonClickedEffect(button) {
@@ -117,13 +126,57 @@ function buttonClickedEffect(button) {
 
 function checkAllShipsPlaced() {
     for (let i = 0; i < playerShips.length; i++) {
-      if (playerShips[i].position.includes(null)) {
+      if (playerShips[i].position.includes(undefined)) {
         return false;
       }
     }
-    return true;
+    console.log(true);
 };
-  
+
+/*-------BUG-------*/
+function hit(e) {
+    let x = e.target.id;
+    if (computerShipArray.includes(x)) {
+      e.target.style.backgroundColor = "#ff0000";
+    } else {
+      e.target.style.backgroundColor = "#ffffff";
+      e.target.fontSize = "20px";
+      e.target.text = "X";
+    }
+
+  setTimeout(computerTurn, 1000);
+    }
+
+function computerTurn() {
+    let x = Math.floor(Math.random() * 9);
+    let y = Math.floor(Math.random() * 9);
+    let coordinate = String.fromCharCode(65 + x) + (y + 1);
+    let hit = false;
+    for (let i = 0; i < playerShips.length; i++) {
+      hit = checkShipHit(playerShips[i], coordinate);
+      if (hit === true) {
+        break;
+      }
+    }
+    if (hit === true) {
+      /*------BUG------*/
+      let hit = document.getElementById(coordinate);
+      hit.style.backgroundColor = "#ff0000";
+      if (checkGameOver() === true) {
+        alert("You lose!");
+      }
+    } else {
+      let miss = document.getElementById(coordinate);
+      console.log(miss);
+      miss.style.backgroundColor = "#ffffff";
+      miss.style.fontSize = "30px";
+      miss.innerHTML = "X";
+    }
+}
+
+
+
+
 function checkShipHit(ship, coordinate) {
     for (let i = 0; i < ship.position.length; i++) {
       if (ship.position[i] === coordinate) {
@@ -140,18 +193,18 @@ function checkShipSunk(ship) {
         return false;
       }}
     ship.sunk = true;
-    return true;
+    ;
 };
   
   
 function checkGameOver() {
-      for (let i = 0; i < playerShips.length; i++) {
-        if (playerShips[i].sunk = false) {
+      for (let i = 0; i < playerShipArray.length; i++) {
+        if (playerShipArray.some(ship => ship !== null)) {
           return false;
         }
       }
-      for (let i = 0; i < computerShips.length; i++) {
-        if (computerShips[i].sunk = false) {
+      for (let i = 0; i < computerShipArray.length; i++) {
+        if (computerShipArray.some(ship => ship !== null)) {
           return false;
         }
       }
@@ -160,28 +213,64 @@ function checkGameOver() {
   
 // 
 function playerShipPlacement(el) {
-    el.addEventListener('click', function (e) {
+  el.addEventListener('click', function (e) {
+    if (hold === 2) {
       let x = e.target.id;
       let y = x.split("");
       let z = y[0].charCodeAt(0) - 65;
       let w = y[1] - 1;
       playerShips[0].position[0] = x;
       playerShips[0].position[1] = String.fromCharCode(65 + (z + 1)) + (w + 1);
+      playerShipArray.push(x, playerShips[0].position[1]);
       let perp = document.getElementById(playerShips[0].position[1]);
-      e.target.style.backgroundColor = "#af1111";
-      perp.style.backgroundColor = "#af1111";
-      if (hold === 3) {
+      e.target.style.backgroundColor = "#5A5A5A";
+      perp.style.backgroundColor = "#5A5A5A";
+   } else if (hold === 3) {
+        let x = e.target.id;
+        let y = x.split("");
+        let z = y[0].charCodeAt(0) - 65;
+        let w = y[1] - 1;
+        playerShips[1].position[0] = x;
+        playerShips[1].position[1] = String.fromCharCode(65 + (z + 1)) + (w + 1);
         playerShips[1].position[2] = String.fromCharCode(65 + (z + 2)) + (w + 1);
+        playerShipArray.push(x, playerShips[1].position[1], playerShips[1].position[2]);
+        let perp = document.getElementById(playerShips[1].position[1]);       
         let perp1 = document.getElementById(playerShips[1].position[2]);
-        perp1.style.backgroundColor = "#af1111";
+        e.target.style.backgroundColor = "#5A5A5A";
+        perp.style.backgroundColor = "#5A5A5A";
+        perp1.style.backgroundColor = "#5A5A5A";
       } else if (hold === 3.5) {
+        let x = e.target.id;
+        let y = x.split("");
+        let z = y[0].charCodeAt(0) - 65;
+        let w = y[1] - 1;
+        playerShips[2].position[0] = x;
+        playerShips[2].position[1] = String.fromCharCode(65 + (z + 1)) + (w + 1);
         playerShips[2].position[2] = String.fromCharCode(65 + (z + 2)) + (w + 1);
-        let perp1 = document.getElementById(playerShips[1].position[2]);
-        perp1.style.backgroundColor = "#af1111";
+        playerShipArray.push(x, playerShips[2].position[1], playerShips[2].position[2]);
+        let perp = document.getElementById(playerShips[2].position[1]);       
+        let perp1 = document.getElementById(playerShips[2].position[2]);
+        e.target.style.backgroundColor = "#5A5A5A";
+        perp.style.backgroundColor = "#5A5A5A";
+        perp1.style.backgroundColor = "#5A5A5A";
       } else if (hold === 4) {
+        let x = e.target.id;
+        let y = x.split("");
+        let z = y[0].charCodeAt(0) - 65;
+        let w = y[1] - 1;
+        playerShips[3].position[0] = x;
+        playerShips[3].position[1] = String.fromCharCode(65 + (z + 1)) + (w + 1);
+        playerShips[3].position[2] = String.fromCharCode(65 + (z + 2)) + (w + 1);
         playerShips[3].position[3] = String.fromCharCode(65 + (z + 3)) + (w + 1);
+        /* ------BUG------*/
+        playerShipArray.push(x, playerShips[3].position[1], playerShips[3].position[2], playerShips[3].position[3]);
+        let perp = document.getElementById(playerShips[3].position[1]);       
+        let perp1 = document.getElementById(playerShips[3].position[2]);
         let perp2 = document.getElementById(playerShips[3].position[3]);
-        perp2.style.backgroundColor = "#af1111";
-    }}
-)};
+        e.target.style.backgroundColor = "#5A5A5A";
+        perp.style.backgroundColor = "#5A5A5A";
+        perp1.style.backgroundColor = "#5A5A5A";
+        perp2.style.backgroundColor = "#5A5A5A";
+    }})
+  };
   
--- script.js
+++ script.js
@@ -7,8 +7,10 @@ let threeTwo = document.querySelector(".three-2");
 let four = document.querySelector(".four");
 let rotate = document.getElementsByClassName(".rotate");
 let playerBoard = document.getElementsByClassName(".player-board");
-let computerBoard = document.getElementsByClassName(".computer-board");
-const el = document.getElementsByClassName(".player-board")[0];
+let computerBoard = document.querySelector(".computer-board");
+const el = document.getElementsByClassName("player-board")[0];
+
+
 
 reset.addEventListener('click', function () {
   location.reload();
@@ -21,21 +23,23 @@ let playerShips = [];
 let computerShips = [];
 let hold = 0;
 
+
 let isClicked = {
   twoClicked: false,
   threeClicked: false,
   threeTwoClicked: false,
   fourClicked: false
 }
+let buttonClicked;
 
 // Event listeners for the Ship buttons
 two.addEventListener('click', function () {
   hold = 2;
   let button = two;
-  let el = button;
+  buttonClicked = isClicked.twoClicked;
   if (isClicked.twoClicked === false) {
-  shipHoverEffect();
-  playerShipPlacement(el)
+  // shipHoverEffect(el);
+  playerShipPlacement(el);
   buttonClickedEffect(button)
   isClicked.twoClicked = true;
   } else {
@@ -47,20 +51,7 @@ three.addEventListener('click', function () {
   let button = three;
   let el = button;
   if (isClicked.threeClicked === false) {
-  shipHoverEffect();
-  playerShipPlacement(el)
-  buttonClickedEffect(button)
-  isClicked.threeClicked = true;
-  } else {
-    alert("You've already placed this ship!");
-}});
-console.log(threeTwo)
-threeTwo.addEventListener('click', function () {
-  hold = 3.5;
-  let button = threeTwo;
-  let el = button;
-  if (isClicked.threeClicked === false) {
-  shipHoverEffect();
+  // shipHoverEffect(el);
   playerShipPlacement(el)
   buttonClickedEffect(button)
   isClicked.threeClicked = true;
@@ -68,12 +59,25 @@ threeTwo.addEventListener('click', function () {
     alert("You've already placed this ship!");
 }});
 
+threeTwo.addEventListener('click', function () {
+  hold = 3.5;
+  let button = threeTwo;
+  let el = button;
+  let buttonClicked = isClicked.threeTwoClicked;
+  if (isClicked.threeTwoClicked === false) {
+  // shipHoverEffect(el);
+  playerShipPlacement(el)
+  buttonClickedEffect(button)
+  isClicked.threeTwoClicked = true;
+  } else {
+    alert("You've already placed this ship!");
+}});
+
 four.addEventListener('click', function () {
   hold = 4;
-  let button = four;
-  let el = button;
+  let button = four;;
   if (isClicked.fourClicked === false) {
-  shipHoverEffect();
+  // shipHoverEffect(el);
   playerShipPlacement(el)
   buttonClickedEffect(button)
   isClicked.fourClicked = true;
@@ -81,9 +85,25 @@ four.addEventListener('click', function () {
     alert("You've already placed this ship!");
 }});
 
+// event listener for an attempted shot
+computerBoard.addEventListener('click', function (e) {
+  let target = e.target;
+  hit(e);
+  checkGameOver()
+
+});
+
+play[0].addEventListener('click', function () {
+  checkAllShipsPlaced()
+  // if (checkAllShipsPlaced() = true) {
+  //   playGame()
+  // }
+});
+
+ 
+
 // Agregation of functions for Game Start
 function startGame() {
-  console.log("Game Started");
   createBoard();
   makeShips();
   placeShips();
